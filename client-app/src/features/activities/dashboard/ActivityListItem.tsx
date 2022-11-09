55d56d6bdd33304b50c35aae2f0ea30e5519b86d
Ivan MSI <arigatory@gmail.com>
2022-11-10 01:22:45 +0300

    styled  UI

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,56 +1,42 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Button, Item, Label } from "semantic-ui-react";
-import { useStore } from "../../../app/api/stores/store";
-import { Activity } from "../../../app/models/activity";
+import { Link } from 'react-router-dom';
+import { Button, Icon, Item, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
 
 interface Props {
-    activity: Activity;
+  activity: Activity;
 }
 
 export default function ActivityListItem({ activity }: Props) {
-    const [target, setTarget] = useState('');
-    const { activityStore } = useStore();
-    const { deleteActivity, loading } = activityStore;
-  
-    function handleActivityDelete(
-      e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-      id: string
-    ) {
-      setTarget(e.currentTarget.name);
-      deleteActivity(id);
-    }
-    
-    return (
-        <Item key={activity.id}>
-        <Item.Content>
-          <Item.Header>{activity.title}</Item.Header>
-          <Item.Meta>{activity.date}</Item.Meta>
-          <Item.Description>
-            {activity.description}
-            <div>
-              {activity.city}, {activity.venue}
-            </div>
-          </Item.Description>
-          <Item.Extra>
-            <Button
-              as={Link}
-              to={`/activities/${activity.id}`}
-              floated="right"
-              content="View"
-              color="blue"
-            />
-            <Button
-              name={activity.id}
-              loading={loading && target === activity.id}
-              onClick={(e) => handleActivityDelete(e, activity.id)}
-              floated="right"
-              content="Delete"
-              color="red"
-            />
-            <Label basic content={activity.category} />
-          </Item.Extra>
-        </Item.Content>
-      </Item>
-        )
+  return (
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item.Image size="tiny" circular src="/assets/user.png" />
+          <Item.Content>
+            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+              {activity.title}
+            </Item.Header>
+            <Item.Description>Hosted by Bob</Item.Description>
+          </Item.Content>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name="clock" /> {activity.date}
+          <Icon name="marker" /> {activity.venue}
+        </span>
+      </Segment>
+      <Segment secondary>Attendees go here</Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          color="teal"
+          floated="right"
+          content="View"
+        />
+      </Segment>
+    </Segment.Group>
+  );
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/api/stores/store';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-calendar/dist/Calendar.css';
+import './app/layout/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
