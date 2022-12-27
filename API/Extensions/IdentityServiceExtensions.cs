687c190aefab09b5a38a3c3ca19a414238c372a5
Ivan MSI <arigatory@gmail.com>
2022-12-27 06:19:08 +0300

    hub auth

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -34,10 +34,23 @@ public static class IdentityServiceExtensions
                     ValidateIssuer = false,
                     ValidateAudience = false,
                 };
+                opt.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
             });
-        services.AddAuthorization(opt => 
+        services.AddAuthorization(opt =>
         {
-            opt.AddPolicy("IsActivityHost", policy => 
+            opt.AddPolicy("IsActivityHost", policy =>
             {
                 policy.Requirements.Add(new IsHostRequirement());
             });
