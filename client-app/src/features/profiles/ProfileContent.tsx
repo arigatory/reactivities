5d93e66fdf015e21ae0ae0d039eda46dd6cd46df
Ivan MSI <arigatory@gmail.com>
2023-04-15 14:50:16 +0300

    done followers

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -4,23 +4,27 @@ import { Tab } from 'semantic-ui-react';
 import { Profile } from '../../app/models/profile';
 import ProfilesPhotos from './ProfilesPhotos';
 import ProfileAbout from './ProfileAbout';
+import ProfileFollowings from './ProfileFollowings';
+import { useStore } from '../../app/stores/store';
 
 interface Props {
   profile: Profile;
 }
 
 export default observer(function ProfileContent({ profile }: Props) {
+  const { profileStore } = useStore();
+
   const panes = [
-    {menuItem: 'About', render: () => <ProfileAbout />}, 
+    { menuItem: 'About', render: () => <ProfileAbout /> },
     { menuItem: 'Photos', render: () => <ProfilesPhotos profile={profile} /> },
     { menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane> },
     {
       menuItem: 'Followers',
-      render: () => <Tab.Pane>Followers Content</Tab.Pane>,
+      render: () => <ProfileFollowings />,
     },
     {
       menuItem: 'Following',
-      render: () => <Tab.Pane>Following Content</Tab.Pane>,
+      render: () => <ProfileFollowings />,
     },
   ];
   return (
@@ -28,6 +32,9 @@ export default observer(function ProfileContent({ profile }: Props) {
       menu={{ fluid: true, vertical: true }}
       menuPosition="right"
       panes={panes}
+      onTabChange={(e, data) =>
+        profileStore.setActiveTab(data.activeIndex as number)
+      }
     />
   );
 });
--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -10,11 +10,12 @@ import ProfileHeader from './ProfileHeader';
 export default observer(function ProfilePage() {
   const { username } = useParams<{ username: string }>();
   const { profileStore } = useStore();
-  const { loadingProfile, loadProfile, profile } = profileStore;
+  const { loadingProfile, loadProfile, profile, setActiveTab } = profileStore;
 
   useEffect(() => {
     if (username) loadProfile(username);
-  }, [username, loadProfile]);
+    return () => setActiveTab(0);
+  }, [username, loadProfile, setActiveTab]);
 
   if (loadingProfile) return <LoadingComponent content="Loading profile..." />;
 
