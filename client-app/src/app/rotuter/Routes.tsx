70b56c3af50162d9c04719941b19f69503a09d0a
Ivan MSI <arigatory@gmail.com>
2023-04-16 14:53:34 +0300

    Pre-deploy

--- client-app/src/app/rotuter/Routes.tsx
+++ client-app/src/app/rotuter/Routes.tsx
@@ -7,18 +7,24 @@ import ServerError from '../../features/errors/ServerError';
 import TestErrors from '../../features/errors/TestError';
 import ProfilePage from '../../features/profiles/ProfilePage';
 import App from '../layout/App';
+import RequireAuth from './RequireAuth';
 
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <App />,
     children: [
-      { path: 'activities', element: <ActivityDashboard /> },
-      { path: 'activities/:id', element: <ActivityDetails /> },
-      { path: 'createActivity', element: <ActivityForm key="create" /> },
-      { path: 'manage/:id', element: <ActivityForm key="manage" /> },
-      { path: 'profiles/:username', element: <ProfilePage key="manage" /> },
-      { path: 'errors', element: <TestErrors /> },
+      {
+        element: <RequireAuth />,
+        children: [
+          { path: 'activities', element: <ActivityDashboard /> },
+          { path: 'activities/:id', element: <ActivityDetails /> },
+          { path: 'createActivity', element: <ActivityForm key="create" /> },
+          { path: 'manage/:id', element: <ActivityForm key="manage" /> },
+          { path: 'profiles/:username', element: <ProfilePage key="manage" /> },
+          { path: 'errors', element: <TestErrors /> },
+        ],
+      },
       { path: 'server-error', element: <ServerError /> },
       { path: 'not-found', element: <NotFound /> },
       { path: '*', element: <Navigate replace to="/not-found" /> },
