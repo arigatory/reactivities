04600577e4054a6bac50437c11e350605eb605bb
Ivan MSI <arigatory@gmail.com>
2023-04-16 16:04:19 +0300

    new connstr

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -41,16 +41,17 @@ public static class ApplicationServiceExtensions
                 var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                 // Parse connection URL to connection string for Npgsql
-                connUrl = connUrl.Replace("postgres://", string.Empty);
-                var pgUserPass = connUrl.Split("@")[0];
-                var pgHostPortDb = connUrl.Split("@")[1];
+                // connUrl = connUrl.Replace("postgres://", string.Empty);
+                // var pgUserPass = connUrl.Split("@")[0];
+                // var pgHostPortDb = connUrl.Split("@")[1];
 
-                var pgUser = "postgres";
-                var pgPass = pgUserPass.Split(":")[1];
-                var pgHost = "reactivities-arigatory-db.fly.dev";
-                var pgPort = "5432";
+                // var pgUser = "postgres";
+                // var pgPass = pgUserPass.Split(":")[1];
+                // var pgHost = "reactivities-arigatory-db.fly.dev";
+                // var pgPort = "5432";
 
-                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database=reactivities;";
+                connStr = "Server=fdaa:1:744a:0:1::4;Port=5432;User Id=postgres;Password=eu5ZHIvz4ehZ0wU;Database=reactivities-arigatory-db;";
+
             }
 
             // Whether the connection string came from the local development configuration file
